c88a4bdf1c50ed0f1aa0a00f7d54e5fe4bfd9f0a
AL EKSAN DITYA PRASETYA <62454581+eksanadit321@users.noreply.github.com>
2022-05-18 12:42:53 +0700

    Add files via upload

---
+++ src/configs/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    multipleStatements  : true,
+    host                : 'localhost',
+    user                : 'eksan',
+    password            : 'njmania321',
+    database            : 'db_node_login'
+};
