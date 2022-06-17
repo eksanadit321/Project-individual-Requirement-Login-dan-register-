2eee8da9ce2d280a37d9ecc27b5922c22f4d1a23
AL EKSAN DITYA PRASETYA <62454581+eksanadit321@users.noreply.github.com>
2022-06-17 16:19:06 +0700

    Delete database.js

--- src/configs/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    multipleStatements  : true,
-    host                : 'localhost',
-    user                : 'eksan',
-    password            : 'njmania321',
-    database            : 'db_node_login'
-};
